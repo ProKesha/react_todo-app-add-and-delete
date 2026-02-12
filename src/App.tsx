1bda867c8927c73abbbdd00cfafb89298bcfc3c3
Dmytro Popov <lort20178@gmail.com>
2026-02-12 11:42:55 +0200

    Move information from previous task

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,74 @@
-/* eslint-disable max-len */
-/* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
-import { UserWarning } from './UserWarning';
-
-const USER_ID = 0;
+import React, { useEffect, useState } from 'react';
+import { Todo } from './types/Todo';
+import { getTodos } from './api/todos';
+import { TodoList } from './components/TodoList';
+import { Footer } from './components/Footer';
+import { NewTodoField } from './components/NewTodoField';
+import { ErrorNotification } from './components/ErrorNotification';
+import { Filter } from './enums/Filter';
 
 export const App: React.FC = () => {
-  if (!USER_ID) {
-    return <UserWarning />;
-  }
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
+  const [filter, setFilter] = useState(Filter.All);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    getTodos()
+      .then(setTodos)
+      .catch(() => {
+        setHasError(true);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
+  const activeCount = todos.filter(todo => !todo.completed).length;
+  const visibleTodos = todos.filter(todo => {
+    switch (filter) {
+      case Filter.Active:
+        return !todo.completed;
+
+      case Filter.Completed:
+        return todo.completed;
+
+      default:
+        return true;
+    }
+  });
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">
-          React Todo App - Load Todos
-        </a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <NewTodoField />
+
+        {!isLoading && todos.length > 0 && <TodoList todos={visibleTodos} />}
+
+        {!isLoading && todos.length > 0 && (
+          <Footer
+            activeCount={activeCount}
+            filter={filter}
+            onFilterChange={setFilter}
+          />
+        )}
+      </div>
+
+      <ErrorNotification
+        isVisible={hasError}
+        onClose={() => setHasError(false)}
+      />
+
+      {isLoading && (
+        <div data-cy="TodoLoader" className="modal overlay">
+          <div className="modal-background has-background-white-ter" />
+          <div className="loader" />
+        </div>
+      )}
+    </div>
   );
 };
